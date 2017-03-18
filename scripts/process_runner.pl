0f8448b5825311d6f25a67ca4a25bd414f04f6d4
Qing Zheng <zhengqmark@gmail.com>
2017-03-18 07:32:41 +0000

    add pdl license

--- scripts/process_runner.pl
+++ scripts/process_runner.pl
@@ -1,9 +1,38 @@
 #!/usr/bin/env perl
+
 #
-# process_runner.pl  process mercury-runner output
-# 03-Mar-2017  chuck@ece.cmu.edu
+# Copyright (c) 2017, Carnegie Mellon University.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the University nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+# HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+# WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 #
 
+#
+# process_runner.pl  process mercury-runner output
+#
 #
 # feed both the client and server output for a set of runs into this
 # and it will produce a short report...  the runs should have the same
@@ -31,9 +60,9 @@ while (<STDIN>) {
         $count = $2;           # number of RPCs we did
         $mode = $3;            # c (client)
         $limit = $4;           # limit on # of outstanding RPCs
-        $run = $5;             # run/iteration number 
+        $run = $5;             # run/iteration number
         $time = "$6.$7";       # run time in seconds
-    
+
         $key = $limit;         # use limit as key
         $max = $key if ($max < $key);  # track largest limit we used
         $kcount{$key}++;
@@ -44,7 +73,7 @@ while (<STDIN>) {
     # ALL-1-100000-c-256-3: ... times sys=4.784000 (secs)
     next unless (/^ALL-(\d+)-(\d+)-(c|s)-(\d+)-(\d+)/);
     $ninst = $1;               # same format as above
-    $count = $2;       
+    $count = $2;
     $mode = $3;
     $limit = $4;
     $run = $5;
@@ -75,7 +104,7 @@ for ($lcv = 1 ; $lcv <= $max ; $lcv = $lcv * 2) {
     $st = $sttot{$key};
     $stime = ($sc) ? sprintf("%f", $st / $sc) : "<none>";
 
-    printf "%s %3d %s sec per op, cli/srv sys time %s / %s sec, r=%d\n", 
+    printf "%s %3d %s sec per op, cli/srv sys time %s / %s sec, r=%d\n",
         $type, $lcv, $kspo, $ctime, $stime, $kc;
 }
 
