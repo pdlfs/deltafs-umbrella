d0b04c37dccc73200e5c895d21b1334938256d72
Chuck Cranor <chuck@ece.cmu.edu>
2017-05-16 16:30:07 -0400

    basic burst buffer test

--- scripts/basic-bb.sh
+++ scripts/basic-bb.sh
@@ -6,13 +6,23 @@
 # DataWarp striped scratch allocation
 #DW jobdw type=scratch access_mode=striped capacity=5GiB optimization_strategy=interference
 
+module load dws
+
 echo "striped $DW_JOB_STRIPED"
 echo "private: $DW_JOB_PRIVATE"
 echo "DW_PERSISTENT_STRIPED_bws=$DW_PERSISTENT_STRIPED_bws"
 
-# No ACDW_JOBID so use the mount directly
-sessid=$(basename $DW_JOB_STRIPED |cut -f 1 -d _)
+# XXX: assume token is encoded in the dir path
+# (at least on slurm the token is the job id)
+dwtoken=$(basename $DW_JOB_STRIPED | cut -f 1 -d _)
+sessid=$(dwstat sessions | \
+                 awk -v token=$dwtoken '{if ($3 == token) print $1}')
+
+echo "DW the token is: $dwtoken"
 echo "DW Session ID: $sessid"
+
+echo "Output of dwstat sessions--look for $sessid for us"
+dwstat sessions
 
 srun -n 1 touch $DW_JOB_STRIPED/file.txt
 srun -n 1 ls $DW_JOB_STRIPED
