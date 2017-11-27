42b184e792cfc8f65711c1d104508edf010f9c17
Chuck Cranor <chuck@ece.cmu.edu>
2017-11-26 22:28:16 -0500

    git subrepo pull umbrella
    subrepo:
      subdir:   "umbrella"
      merged:   "11aa64e"
    upstream:
      origin:   "git@dev.pdl.cmu.edu:pdlfs/umbrella"
      branch:   "master"
      commit:   "29b479e"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ umbrella/umbrella/abt-io.cmake
@@ -0,0 +1,57 @@
+#
+# abt-io.cmake  umbrella for ABT-IO package
+# 06-Nov-2017  chuck@ece.cmu.edu
+#
+
+#
+# config:
+#  ABT_IO_REPO - url of git repository
+#  ABT_IO_TAG  - tag to checkout of git
+#  ABT_IO_TAR  - cache tar file name (default should be ok)
+#
+
+if (NOT TARGET abt-io)
+
+#
+# umbrella option variables
+#
+umbrella_defineopt (ABT_IO_REPO "https://xgitlab.cels.anl.gov/sds/abt-io.git"
+     STRING "ABT_IO GIT repository")
+umbrella_defineopt (ABT_IO_TAG "master" STRING "ABT_IO GIT tag")
+umbrella_defineopt (ABT_IO_TAR "abt-io-${ABT_IO_TAG}.tar.gz"
+                    STRING "ABT_IO cache tar file")
+
+#
+# generate parts of the ExternalProject_Add args...
+#
+umbrella_download (ABT_IO_DOWNLOAD abt-io ${ABT_IO_TAR}
+                   GIT_REPOSITORY ${ABT_IO_REPO}
+                   GIT_TAG ${ABT_IO_TAG})
+umbrella_patchcheck (ABT_IO_PATCHCMD abt-io)
+
+# abt-io requirements
+include (umbrella/argobots)
+
+#
+# create abt-io target
+#
+ExternalProject_Add (abt-io DEPENDS argobots
+    ${ABT_IO_DOWNLOAD} ${ABT_IO_PATCHCMD}
+    CONFIGURE_COMMAND <SOURCE_DIR>/configure ${UMBRELLA_MPICOMP}
+                      ${UMBRELLA_CPPFLAGS} ${UMBRELLA_LDFLAG}
+                      ${UMBRELLA_PKGCFGPATH}
+                      --prefix=${CMAKE_INSTALL_PREFIX}
+                      --enable-shared
+    UPDATE_COMMAND "")
+
+#
+# add extra autogen prepare step
+#
+ExternalProject_Add_Step (abt-io prepare
+    COMMAND ${UMBRELLA_PREFIX}/ensure-autogen <SOURCE_DIR>/prepare.sh
+    COMMENT "preparing source for configure"
+    DEPENDEES update
+    DEPENDERS configure
+    WORKING_DIRECTORY <SOURCE_DIR>)
+
+endif (NOT TARGET abt-io)
---
+++ umbrella/umbrella/sds-keyval.cmake
@@ -0,0 +1,57 @@
+#
+# sds-keyval.cmake  umbrella for sds-keyval package
+# 06-Nov-2017  chuck@ece.cmu.edu
+#
+
+#
+# config:
+#  SDS_KEYVAL_REPO - url of git repository
+#  SDS_KEYVAL_TAG  - tag to checkout of git
+#  SDS_KEYVAL_TAR  - cache tar file name (default should be ok)
+#
+
+if (NOT TARGET sds-keyval)
+
+#
+# umbrella option variables
+#
+umbrella_defineopt (SDS_KEYVAL_REPO "https://xgitlab.cels.anl.gov/sds/sds-keyval.git"
+     STRING "SDS_KEYVAL GIT repository")
+umbrella_defineopt (SDS_KEYVAL_TAG "master" STRING "SDS_KEYVAL GIT tag")
+umbrella_defineopt (SDS_KEYVAL_TAR "sds-keyval-${SDS_KEYVAL_TAG}.tar.gz"
+                    STRING "SDS_KEYVAL cache tar file")
+
+#
+# generate parts of the ExternalProject_Add args...
+#
+umbrella_download (SDS_KEYVAL_DOWNLOAD sds-keyval ${SDS_KEYVAL_TAR}
+                   GIT_REPOSITORY ${SDS_KEYVAL_REPO}
+                   GIT_TAG ${SDS_KEYVAL_TAG})
+umbrella_patchcheck (SDS_KEYVAL_PATCHCMD sds-keyval)
+
+# sds-keyval requirements
+include (umbrella/margo)
+
+#
+# create sds-keyval target
+#
+ExternalProject_Add (sds-keyval DEPENDS margo
+    ${SDS_KEYVAL_DOWNLOAD} ${SDS_KEYVAL_PATCHCMD}
+    CONFIGURE_COMMAND <SOURCE_DIR>/configure ${UMBRELLA_MPICOMP}
+                      ${UMBRELLA_CPPFLAGS} ${UMBRELLA_LDFLAG}
+                      ${UMBRELLA_PKGCFGPATH}
+                      --prefix=${CMAKE_INSTALL_PREFIX}
+                      --enable-shared
+    UPDATE_COMMAND "")
+
+#
+# add extra autogen prepare step
+#
+ExternalProject_Add_Step (sds-keyval prepare
+    COMMAND ${UMBRELLA_PREFIX}/ensure-autogen <SOURCE_DIR>/prepare.sh
+    COMMENT "preparing source for configure"
+    DEPENDEES update
+    DEPENDERS configure
+    WORKING_DIRECTORY <SOURCE_DIR>)
+
+endif (NOT TARGET sds-keyval)
