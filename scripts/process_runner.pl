88c86152d96694e1705edad5c6f712e24fc3e3b5
Chuck Cranor <chuck@ece.cmu.edu>
2018-01-22 22:08:34 -0500

    revise output format to include more timing information and document the script a little better.

--- scripts/process_runner.pl
+++ scripts/process_runner.pl
@@ -43,7 +43,10 @@ use strict;
 
 my($type, $ninst, $count, $mode, $limit, $run, $time);
 my($key, $max, $lcv);
-my(%modes, %kcount, %ktot, %stcount, %sttot, $bi);
+my(%modes);                    # what modes we've seen (for sanity check)
+my(%nsecperop, %secperopsum);  # total# of sec/op seen, sum of sec per op
+my(%stcount, %wallsum, %usrsum, %syssum);  # stat count, sums of times
+my($walltime, $usrtime, $systime, $bi);
 
 while (<STDIN>) {
     chop;
@@ -61,13 +64,13 @@ while (<STDIN>) {
         $mode = $3;            # c (client)
         $limit = $4;           # limit on # of outstanding RPCs
         $run = $5;             # run/iteration number
-        $time = "$6.$7";       # run time in seconds
+        $time = "$6.$7";       # sec per op
 
         $modes{$mode} = 1;     # for sanity check
         $key = $limit;         # use limit as key
         $max = $key if ($max < $key);  # track largest limit we used
-        $kcount{$key}++;
-        $ktot{$key} += $time;
+        $nsecperop{$key}++;
+        $secperopsum{$key} += $time;
         next;
     }
 
@@ -78,13 +81,19 @@ while (<STDIN>) {
     $mode = $3;
     $limit = $4;
     $run = $5;
-
+    next unless (/wall=(\d+)\.(\d+)/);
+    $walltime = "$1.$2";
+    next unless (/usr=(\d+)\.(\d+)/);
+    $usrtime = "$1.$2";
     next unless (/sys=(\d+)\.(\d+)/);
-    $modes{$mode} = 1;         # for sanity check
-    $time = "$1.$2";
+    $systime = "$1.$2";
+
     $key = "$mode-$limit";     # key include client/server and limit
+    $modes{$mode} = 1;         # for sanity check
     $stcount{$key}++;
-    $sttot{$key} += $time;
+    $wallsum{$key} += $walltime;
+    $usrsum{$key} += $usrtime;
+    $syssum{$key} += $systime;
 }
 
 # do some sanity checks
@@ -95,7 +104,7 @@ if ($bi && ($modes{'c'} || $modes{'s'})) {
 }
 if ($bi) {
     for ($lcv = 1 ; $lcv <= $max ; $lcv = $lcv * 2) {
-        if ($kcount{"$lcv"} & 1) {
+        if ($nsecperop{"$lcv"} & 1) {
             printf STDERR "process_runner.pl: ERROR: odd# of bidir results\n";
             exit(1);
         }
@@ -104,30 +113,41 @@ if ($bi) {
 
 
 for ($lcv = 1 ; $lcv <= $max ; $lcv = $lcv * 2) {
-    my($kc, $kt, $kspo);
-    my($cc, $ct, $ctime, $sc, $st, $stime);
+    my($nresults, $resultsum, $avgspo);
+    my($statcnt, $cwall, $cusr, $csys, $swall, $susr, $ssys);
 
     $key = "$lcv";
-    $kc = $kcount{$key};
-    $kt = $ktot{$key};
-    $kspo = ($kc) ? sprintf("%f", $kt / $kc) : "<none>";
+    $nresults = $nsecperop{$key};
+    $resultsum = $secperopsum{$key};
+    $avgspo = ($nresults) ? sprintf("%f", $resultsum / $nresults) : "<none>";
 
-    $key = ($bi) ? "cs-$lcv" : "c-$lcv";
-    $cc = $stcount{$key};
-    $ct = $sttot{$key};
-    $ctime = ($cc) ? sprintf("%f", $ct / $cc) : "<none>";
+    $key = ($bi) ? "cs-$lcv" : "c-$lcv";   # for bi-dir, use client vars
+    $statcnt = $stcount{$key};
+    if ($statcnt) {
+        $cwall = sprintf("%f", $wallsum{$key} / $statcnt);
+        $cusr  = sprintf("%f", $usrsum{$key}  / $statcnt);
+        $csys  = sprintf("%f", $syssum{$key}  / $statcnt);
+    } else {
+        $cwall = $cusr = $csys = "<none>";
+    }
 
     $key = "s-$lcv";
-    $sc = $stcount{$key};
-    $st = $sttot{$key};
-    $stime = ($sc) ? sprintf("%f", $st / $sc) : "<none>";
+    $statcnt = $stcount{$key};
+    if ($statcnt) {
+        $swall = sprintf("%f", $wallsum{$key} / $statcnt);
+        $susr  = sprintf("%f", $usrsum{$key}  / $statcnt);
+        $ssys  = sprintf("%f", $syssum{$key}  / $statcnt);
+    } else {
+        $swall = $susr = $ssys = "<none>";
+    }
 
     if ($bi) {
-        printf "%s %3d %s sec per op, sys time %s sec, r=%d\n",
-            $type, $lcv, $kspo, $ctime, $kc/2;
+        printf "%s %3d %s sec/op, %.2fs, u/sys  %.2f/%.2f, r=%d\n",
+            $type, $lcv, $avgspo, $cwall, $cusr, $csys, $nresults/2;
     } else {
-        printf "%s %3d %s sec per op, cli/srv sys time %s / %s sec, r=%d\n",
-            $type, $lcv, $kspo, $ctime, $stime, $kc;
+        printf "%s %3d %s sec/op, %.2fs, u/sys  " .
+               "c=%.2f/%.2f  s=%.2f/%.2f, r=%d\n",
+            $type, $lcv, $avgspo, $cwall, $cusr, $csys, $susr, $ssys, $nresults;
     }
 }
 
