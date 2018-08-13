cb61181ec728fccaa6ab4e5e4f414abcf3baa86d
Qing Zheng <zhengqmark@gmail.com>
2018-08-13 17:07:59 +0000

    update mercury to include verbose error and stats settings

--- umbrella/umbrella/mercury.cmake
+++ umbrella/umbrella/mercury.cmake
@@ -12,6 +12,8 @@
 #  MERCURY_OPA - force use of OPA atomic lib
 #  MERCURY_POST_LIMIT - enable post limit
 #  MERCURY_SELF_FORWARD - enable self forward thread
+#  MERCURY_VERBOSE_ERROR - enable verbose error
+#  MERCURY_STATS - enable stats
 #  MERCURY_CHECKSUM - enable checksuming
 #
 #  MERCURY_NA_INITIALLY_ON - cmake list of NAs that are initally enabled
@@ -37,6 +39,8 @@ umbrella_defineopt (MERCURY_TAR "mercury-${MERCURY_TAG}.tar.gz"
 umbrella_defineopt (MERCURY_OPA "OFF" BOOL "Force use of OPA atomic lib")
 umbrella_defineopt (MERCURY_POST_LIMIT "ON" BOOL "Enable post limit")
 umbrella_defineopt (MERCURY_SELF_FORWARD "OFF" BOOL "Enable self forward thread")
+umbrella_defineopt (MERCURY_VERBOSE_ERROR "ON" BOOL "Enable verbose error")
+umbrella_defineopt (MERCURY_STATS "OFF" BOOL "Enable stats reporting")
 umbrella_defineopt (MERCURY_CHECKSUM "OFF" BOOL "Enable checksuming")
 
 #
@@ -68,6 +72,8 @@ set (MERCURY_CMAKE_ARGS -DNA_USE_MPI=OFF -DNA_USE_SM=${MERCURY_SM}
      -DMERCURY_USE_SELF_FORWARD:BOOL=${MERCURY_SELF_FORWARD}
      -DMERCURY_ENABLE_POST_LIMIT:BOOL=${MERCURY_POST_LIMIT}
      -DMERCURY_USE_BOOST_PP=ON -DMERCURY_USE_CHECKSUMS:BOOL=${MERCURY_CHECKSUM}
+     -DMERCURY_ENABLE_VERBOSE_ERROR:BOOL=${MERCURY_VERBOSE_ERROR}
+     -DMERCURY_ENABLE_STATS:BOOL=${MERCURY_STATS}
      -DNA_USE_BMI=${MERCURY_BMI} -DNA_USE_CCI=${MERCURY_CCI}
      -DNA_USE_OFI=${MERCURY_OFI})
 
@@ -99,6 +105,8 @@ endif (MERCURY_OFI)
 #
 message (STATUS "  Mercury config:")
 message (STATUS "    HG self-forward: ${MERCURY_SELF_FORWARD}")
+message (STATUS "    HG verbose: ${MERCURY_VERBOSE_ERROR}")
+message (STATUS "    HG stats: ${MERCURY_STATS}")
 message (STATUS "    HG post limit: ${MERCURY_POST_LIMIT}")
 message (STATUS "    HG force OPA: ${MERCURY_OPA}")
 message (STATUS "    NAs: bmi=${MERCURY_BMI} cci=${MERCURY_CCI}")
