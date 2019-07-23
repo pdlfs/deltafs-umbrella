315a3b9c5ab38ea6a28a0968ac5e467c0294db7b
Chuck Cranor <chuck@ece.cmu.edu>
2019-07-23 10:13:23 -0400

    update vpic for our version of the master tree.

--- umbrella/umbrella/vpic.cmake
+++ umbrella/umbrella/vpic.cmake
@@ -1,6 +1,6 @@
 #
 # vpic.cmake  umbrella for vpic simulator
-# 27-Sep-2017  chuck@ece.cmu.edu
+# 12-Jul-2019  chuck@ece.cmu.edu
 #
 
 #
@@ -8,6 +8,7 @@
 #  VPIC_REPO - url of git repository
 #  VPIC_TAG  - tag to checkout of git
 #  VPIC_TAR  - cache tar file name (default should be ok)
+#  VPIC_CFG  - cache config script
 #
 
 if (NOT TARGET vpic)
@@ -20,6 +21,7 @@ umbrella_defineopt (VPIC_REPO "https://github.com/pdlfs/vpic.git"
 umbrella_defineopt (VPIC_TAG "master" STRING "VPIC GIT tag")
 umbrella_defineopt (VPIC_TAR "vpic-${VPIC_TAG}.tar.gz"
      STRING "VPIC cache tar file")
+umbrella_defineopt (VPIC_CFG "gcc/v4-sse" STRING "VPIC cache config script")
 
 #
 # generate parts of the ExternalProject_Add args...
@@ -29,40 +31,23 @@ umbrella_download (VPIC_DOWNLOAD vpic ${VPIC_TAR}
                    GIT_TAG ${VPIC_TAG})
 umbrella_patchcheck (VPIC_PATCHCMD vpic)
 
-# XXX: doesn't really depend on deltafs, but we want to build that first
-include (umbrella/deltafs)
-
 #
 # create vpic target
 #
 ExternalProject_Add (vpic ${VPIC_DOWNLOAD} ${VPIC_PATCHCMD}
-
-    #
-    # TODO: This code makes me sad, but then again this VPIC version
-    #       also makes me sad. Feel free to fix.
-
-    CONFIGURE_COMMAND cd <SOURCE_DIR> && ./config/bootstrap
-        COMMAND <SOURCE_DIR>/prep_conf.sh <SOURCE_DIR>
-        COMMAND <SOURCE_DIR>/configure ${UMBRELLA_MPICOMP}
-                --prefix=${CMAKE_INSTALL_PREFIX}
-                --with-machine=<SOURCE_DIR>/machine.conf
-
-    # We only compile a sample deck here to ensure build.op was properly
-    # put together.
-
-    BUILD_COMMAND make
-        COMMAND cd <SOURCE_DIR> &&
-        <BINARY_DIR>/build.op decks/trecon-part/turbulence.cxx &&
-        <BINARY_DIR>/build.op decks/fan-run/turbulence-sheet-tracer2spec.cxx
-
-    INSTALL_COMMAND make install
-        COMMAND rm -rf ${CMAKE_INSTALL_PREFIX}/decks
-        COMMAND cp -r <SOURCE_DIR>/decks ${CMAKE_INSTALL_PREFIX}
-        COMMAND cp -r <SOURCE_DIR>/src/deck_wrapper.cxx
-                                  ${CMAKE_INSTALL_PREFIX}/decks
-        COMMAND cp -r <SOURCE_DIR>/src/main.cxx ${CMAKE_INSTALL_PREFIX}/decks
-        COMMAND cp -r <BINARY_DIR>/vpic-build.op ${CMAKE_INSTALL_PREFIX}/bin
+    CMAKE_ARGS -C ./vpic-init-cache.cmake -DBUILD_SHARED_LIBS=ON
+    CMAKE_CACHE_ARGS ${UMBRELLA_CMAKECACHE}
     UPDATE_COMMAND ""
 )
+
+#
+# add extra cache config step
+#
+ExternalProject_Add_Step (vpic prepare
+    COMMAND <SOURCE_DIR>/arch/${VPIC_CFG} initcache
+    COMMENT "generating initial cmake cache for configure ${VPIC_CFG}"
+    DEPENDEES update
+    DEPENDERS configure
+    WORKING_DIRECTORY <BINARY_DIR>)
 
 endif (NOT TARGET vpic)
